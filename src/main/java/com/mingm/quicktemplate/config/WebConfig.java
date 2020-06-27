682a47fc3461cb02e94a535dd536440b0b68f459
MingmPanda <mingm.panda@gmail.com>
2020-06-28 03:15:44 +0800

    集成Guava令牌桶实现全局限流

---
+++ src/main/java/com/mingm/quicktemplate/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.mingm.quicktemplate.config;
+
+import com.mingm.quicktemplate.interceptor.RateLimitInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author: panmm
+ * @date: 2020/6/28 03:11
+ * @description: Web配置类
+ */
+@Configuration
+@EnableWebMvc
+@Slf4j
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 全局限流拦截器
+     */
+    @Resource
+    private RateLimitInterceptor rateLimitInterceptor;
+
+    /**
+     * 向Web中添加拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        // 配置限流拦截器，拦截所有以/api/开头的请求
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api/**");
+    }
+}
