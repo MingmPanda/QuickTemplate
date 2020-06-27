d50b19f8aa2935bba1e8ec1e386130cd84abf272
MingmPanda <mingm.panda@gmail.com>
2020-06-27 19:30:30 +0800

    first commit

---
+++ src/main/java/com/mingm/quicktemplate/QuickTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mingm.quicktemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mingm/quicktemplate/QuickTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingm.quicktemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
